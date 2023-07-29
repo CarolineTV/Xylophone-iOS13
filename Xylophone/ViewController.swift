5c7be54ae063cdd1d53aad1af92ddf9f7fb2a2d6
Caroline Tikhomirova <sonyukcv@gmail.com>
2023-07-29 13:31:02 +0400

    add changing opacity of button

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -22,7 +22,12 @@ class ViewController: UIViewController {
         
         
         playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            sender.alpha = 1.0
+            
+        }
     }
     
     func playSound(soundName: String) {
